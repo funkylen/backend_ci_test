81f885bd346dee490b99810693090f4f6b851612
funkylen <funkylen@gmail.com>
2020-06-13 21:56:56 +0300

    Исключение логов из git

--- application/logs/log-2020-06-13.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2020-06-13 16:51:11 --> 404 Page Not Found: Faviconico/index
-ERROR - 2020-06-13 16:53:49 --> 404 Page Not Found: Faviconico/index
