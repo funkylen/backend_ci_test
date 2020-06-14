f9f479abb7f61d4727a17f95cdde5984b324b969
funkylen <funkylen@gmail.com>
2020-06-14 14:37:37 +0300

    Добавление дампа

---
+++ mysql/dumps/second_dump.sql
@@ -0,0 +1,20 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `post_likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `amount` int(11) UNSIGNED NOT NULL,
+  `time_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `time_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`post_id`) REFERENCES post(`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`user_id`) REFERENCES user(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+COMMIT;
